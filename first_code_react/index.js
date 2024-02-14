3d85dcb175ea5f234e503a8f0a3791e8037fb6ce
maxamediidg <maxamedeid72@gmail.com>
2024-02-14 15:46:26 +0300

    commit🚀🚀

---
+++ first_code_react/index.js
@@ -0,0 +1,11 @@
+// ReactDOM.render(<p>Hello world form react</p>,document.getElementById("root"));
+// ReactDOM.render(<div><h>React Course</h><p>We will learn react in this course.</p></div>, document.getElementById("root"));
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <div>
+        <h1>React Course</h1>
+        <p>We will learn react in this course.</p>
+        </div>
+);
